a0b2ef30a13cf153aba1cfabc13af213ce5fc8e6
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-06-10 22:19:30 -0700

    Delete and Paste

--- AlpacaTradingAPI/__init__.py
+++
@@ -1,4 +0,0 @@
-from AlpacaTradingAPI import rest  # noqa
-from .stream2 import StreamConn  # noqa
-
-__version__ = '0.48'
--- main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:847ba9622fa12ed3fd188af8f01ec46271204a5c1700ec142ebb43fe0574cb21
-size 383
